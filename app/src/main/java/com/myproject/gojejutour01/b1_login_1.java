a542df19c615434ed450f4e35bc7e6af0a324c3f
hyojin <dlgywls205@naver.com>
2021-12-07 14:29:01 +0900

    안녕하세요

--- app/src/main/java/com/myproject/gojejutour01/b1_login_1.java
+++ app/src/main/java/com/myproject/gojejutour01/b1_login_1.java
@@ -10,5 +10,6 @@ public class b1_login_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.b1_2_login_1);
+        //안녕하세요
     }
 }
