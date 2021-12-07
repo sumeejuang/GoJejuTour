54c7cd1344d971540a115aaf064adebd343120f2
sumeejuang <87522688+sumeejuang@users.noreply.github.com>
2021-12-07 12:38:42 +0900

    layout추가

---
+++ app/src/main/java/com/myproject/gojejutour01/b0_firstpage_0.java
@@ -0,0 +1,14 @@
+package com.myproject.gojejutour01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class b0_firstpage_0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b0_1_firstpage_0);
+    }
+}
---
+++ app/src/main/java/com/myproject/gojejutour01/b1_login_1.java
@@ -0,0 +1,14 @@
+package com.myproject.gojejutour01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class b1_login_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b1_2_login_1);
+    }
+}
